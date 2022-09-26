4eb18cf2999aecca588dd1f4420ae7a004f967b2
Igor Polousov <igorpolousov@gmail.com>
2022-09-26 14:22:19 +0300

    Initial Commit

---
+++ ItineraryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItineraryApp
+//
+//  Created by Igor Polousov on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
