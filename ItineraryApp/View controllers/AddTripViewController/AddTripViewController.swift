d2074c61100e18b5651daf5bc36be37ad5494cad
Igor Polousov <igorpolousov@gmail.com>
2022-10-04 14:10:12 +0300

    + add view elements: cancel and save buttons text field text label + outlets for elements + actions for both buttons

--- ItineraryApp/View controllers/AddTripViewController/AddTripViewController.swift
+++ ItineraryApp/View controllers/AddTripViewController/AddTripViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class AddTripViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tripTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveAction(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
-
+    
+    @IBAction func cancelAction(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
 }
