a827ac6d109fb6010364c70e3b19e56034b20f5d
Igor Polousov <igorpolousov@gmail.com>
2022-10-05 16:53:59 +0300

    + check if user enter some text in triptextfield

--- ItineraryApp/View controllers/AddTripViewController/AddTripViewController.swift
+++ ItineraryApp/View controllers/AddTripViewController/AddTripViewController.swift
@@ -25,10 +25,30 @@ class AddTripViewController: UIViewController {
     }
     
     @IBAction func saveAction(_ sender: UIButton) {
+        tripTextField.rightViewMode = .never
         
-        if let text = tripTextField.text {
-            TripFunctions.createTrip(tripModel: TripModel(title: text))
+        guard tripTextField.text != "", let text = tripTextField.text  else {
+                      //Show warning image if no text were entered
+//            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+//            imageView.image = UIImage(named: "warning")
+//            tripTextField.rightView = imageView
+            
+            // Alternatives
+            
+            // Чтобы показать пользователю, что он не ввёл текс можно изменить цвет поля
+            //tripTextField.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1) // for adding such a color use = #colorLiteral(
+            
+            // Сделать цвет рамки другого цвета
+            tripTextField.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            tripTextField.layer.borderWidth = 1
+            tripTextField.layer.cornerRadius = 4
+            tripTextField.rightViewMode = .always
+        
+            return
         }
+        
+        TripFunctions.createTrip(tripModel: TripModel(title: text))
+                                 
         if let doneSavings = doneSavings {
             doneSavings()
         }
