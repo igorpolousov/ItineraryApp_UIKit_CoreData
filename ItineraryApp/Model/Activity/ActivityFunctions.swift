99e49a0f66db09f6d6d1fec1cd5b39a64b8ae335
Igor Polousov <igorpolousov@gmail.com>
2022-12-16 21:38:06 +0300

    + update activity + reorder activity

--- ItineraryApp/Model/Activity/ActivityFunctions.swift
+++ ItineraryApp/Model/Activity/ActivityFunctions.swift
@@ -21,4 +21,30 @@ class ActivityFunctions {
         }
     }
     
+    static func updateActivity(at tripIndex: Int, oldDayIndex: Int, newDayIndex: Int, using activityModel: ActivityModel) {
+        // replace with real data store here
+        if oldDayIndex != newDayIndex {
+            //move activity to a different day
+            let lastIndex = Data.tripModels[tripIndex].dayModels[newDayIndex].activities.count
+            reorderActivity(at: tripIndex, oldDayIndex: oldDayIndex, newDayIndex: newDayIndex, newActivityIndex: lastIndex, activityModel: activityModel)
+        } else {
+            //update activity in the same day
+            let dayModel = Data.tripModels[tripIndex].dayModels[oldDayIndex]
+            let activityIndex = (dayModel.activities.firstIndex(of: activityModel))
+            Data.tripModels[tripIndex].dayModels[newDayIndex].activities[activityIndex!] = activityModel
+        }
+    }
+    
+    static func reorderActivity(at tripIndex: Int, oldDayIndex: Int, newDayIndex: Int, newActivityIndex: Int, activityModel: ActivityModel) {
+        // Replace with real data store
+        
+        // 1. Remove activity from old location
+        let dayModel = Data.tripModels[tripIndex].dayModels[oldDayIndex]
+        let oldActivityIndex = (dayModel.activities.firstIndex(of: activityModel))
+        Data.tripModels[tripIndex].dayModels[newDayIndex].activities.remove(at: oldDayIndex)
+        
+        // 2. Insert activity to a new location
+        Data.tripModels[tripIndex].dayModels[newDayIndex].activities.insert(activityModel, at: newDayIndex)
+    }
+    
 }
