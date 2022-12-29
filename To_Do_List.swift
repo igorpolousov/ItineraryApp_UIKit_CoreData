f0fc85e984f89092f24cf3fbdbd868a6b93bfd72
Igor Polousov <igorpolousov@gmail.com>
2022-12-29 16:01:37 +0300

    new Core Data entities model

--- To_Do_List.swift
+++ To_Do_List.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Igor Polousov on 21.12.2022.
 //
-
+// 0. Create new Entities and class files accordingly to a Core Data model in Data.swift file
 // 1. Add additional color theme ( blue gray light blue)
 // 2. Make correct selection on pickers
 // 3. Make more helpful hints
-// 4. Add Core Data
+// 4. Add Core Data:
+// - add core data files
+// - change code in CRUD functions of Trip, Day, Activity
 // 5. Add GCD
 // 6. Add Tests
 
