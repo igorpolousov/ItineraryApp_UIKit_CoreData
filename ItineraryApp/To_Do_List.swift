4870f7cd4fe6cb6885c690e705d7f80154a4d92a
Igor Polousov <igorpolousov@gmail.com>
2023-01-25 13:54:52 +0300

    + comments

--- ItineraryApp/To_Do_List.swift
+++ ItineraryApp/To_Do_List.swift
@@ -7,5 +7,7 @@
 
 // 1. Add additional color theme ( blue gray light blue)
 // 2. Make correct selection on pickers
-// 3. Make more helpful hints
+// 3. Make more helpful hints?
+// 4. Code revision update activity
+// 5. Cloud
 
